750d4f01ad55f8ff0b37586f4c94c3513e3f3be4
jme98 <jennifer.easter@eagles.oc.edu>
2020-03-26 14:39:41 -0500

    env and django added, site created

---
+++ b2b_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\DraconaMachina\Documents\SE5-Repo-CrunchTimeB2B\b2b_env\Scripts\python3.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
