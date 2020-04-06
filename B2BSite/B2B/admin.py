dcab12f58104dbbeea30182a179dd3e3e7dcb152
jme98 <jennifer.easter@eagles.oc.edu>
2020-04-06 14:44:49 -0500

    asdfghjkl

--- B2BSite/B2B/admin.py
+++ B2BSite/B2B/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Company, Person
 
 class CompanyAdmin(admin.ModelAdmin):
-    
+    pass
 
 admin.site.register(Company)
 admin.site.register(Person)
--- B2BSite/B2B/models.py
+++ B2BSite/B2B/models.py
@@ -2,22 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Company(models.Model):
-    company_name = models.CharField(max_length=100)
-    primary_contact = models.ForeignKey(User, on_delete=models.SET_NULL)
 
-    wants_tb = models.BooleanField() #do they care about the Test Bookstore; NO REAL COMPANY WILL - THIS IS JUST HERE FOR TESTING PURPOSES
-    wants_kb = models.BooleanField() #do they care about Kobo
-    wants_gb = models.BooleanField() #do they care about Google Books
-    wants_lc = models.BooleanField() #do they care about Livraria Cultura
-    wants_sd = models.BooleanField() #do they care about Scribd
-    wants_ab = models.BooleanField() #do they care about Audiobooks.com
+    company_name = models.CharField(max_length=100)
+    primary_contact = models.ForeignKey('Person', related_name='+', null=True, on_delete=models.SET_NULL)
+
+    wants_tb = models.BooleanField(verbose_name='Test Bookstore (for TESTING purposes ONLY)') #NO REAL COMPANY WILL WANT THIS
+    wants_kb = models.BooleanField(verbose_name='Kobo')
+    wants_gb = models.BooleanField(verbose_name='Google Books')
+    wants_lc = models.BooleanField(verbose_name='Livraria Cultura')
+    wants_sd = models.BooleanField(verbose_name='Scribd')
+    wants_ab = models.BooleanField(verbose_name='Audiobooks.com')
 
     def get_total_searches(self):
-        for u in :
+        total = 0
+        for p in self.person_set.all():
+            total = total + p.search_count
+        return total
 
+    class Meta:
+        verbose_name_plural = "Companies"
 
-class CompanyUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT) #disallow deletion of users; for auditing purposes it's safer to just go into the admin & remove the user's permissions
-    company =  models.ForeignKey(Company, on_delete=models.PROTECT) #disallow deletion of companies; see previous comment
-    phone = models.CharField(max_length=20, null=True) #20 chars allows international numbers which can be long, nullable because not every user has a phone number we care about
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT) #disallow deletion of users; for auditing purposes it's safer to just go into the admin & uncheck "Active" for the user
+    company =  models.ForeignKey('Company', on_delete=models.PROTECT) #disallow deletion of companies; for auditing purposes it's safer to just deactivate all users at the company
+    phone = models.CharField(max_length=20, null=True) #20 chars allows international numbers which can be long, nullable because not everyone has a phone number we care about
     search_count = models.IntegerField(default=0)
