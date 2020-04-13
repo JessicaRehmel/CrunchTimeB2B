4e2b95f3177bb4bc63a657db71076d49ba87f2ab
Rylenthedragon <rtd7@cox.net>
2020-04-13 18:26:34 -0500

    stuff appears on base_generic but not index; that's the current issue, but a step forward either way

--- B2BSite/B2B/views.py
+++ B2BSite/B2B/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
+from django.views import generic
 # Create your views here.
+
+def index(request):
+    #all_books = Book.objects.all().order_by('title')
+
+    context = {
+        #'all_books': all_books,
+    }
+    return render(request, 'index.html', context = context)
