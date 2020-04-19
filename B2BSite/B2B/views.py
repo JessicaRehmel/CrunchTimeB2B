874691eaae94f8f234a93e4de7fb37c9258d9cba
Rylenthedragon <rtd7@cox.net>
2020-04-19 02:57:12 -0500

    not borked

--- B2BSite/B2B/views.py
+++ B2BSite/B2B/views.py
@@ -15,19 +15,45 @@ def index(request):
 
     
 def results(request):
-    #all_books = Book.objects.all().order_by('title')
+    book_title = "stuff"
+    book_authors = "list of stuff"
+    book_ISBN = "a long number"
+    book_match_percentage = "a fancy number"
+    #book_JSON = "dear lord the text"
 
     context = {
-        #'all_books': all_books,
+        'book_title': book_title,
+        'book_authors': book_authors,
+        'book_ISBN': book_ISBN,
+        'book_match_percentage': book_match_percentage,
+        #'book_JSON': book_JSON,
     }
     return render(request, 'results.html', context = context)
 
 
 def book_detail(request):
-    #all_books = Book.objects.all().order_by('title')
+    #book_title = "stuff"
+    #book_subtitle = "lesser stuff"
+    #book_series = "even lesser stuff"
+    #book_volume = "a number"
+    #book_authors = "a list of stuff"
+    #book_format = "fancy stuff"
+    #book_ISBN = "a long number"
+    #book_url = "complex stuff"
+    #book_match_percentage = "a fancy number"
+    #book_description = "dear lord the text"
 
     context = {
-        #'all_books': all_books,
+        #'book_title': book_title,
+        #'book_subtitle': book_subtitle,
+        #'book_series': book_series,
+        #'book_volume': book_volume,
+        #'book_authors': book_authors,
+        #'book_format': book_format,
+        #'book_ISBN': book_ISBN,
+        #'book_url': book_url,
+        #'book_match_percentage': book_match_percentage,
+        #'book_description': book_description,
     }
     return render(request, 'results.html', context = context)
 
