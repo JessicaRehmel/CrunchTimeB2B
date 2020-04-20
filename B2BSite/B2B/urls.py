8ee738093dbb876dacdc6a0d4fd03f20b3f022a8
jme98 <jennifer.easter@eagles.oc.edu>
2020-04-20 00:44:33 -0500

    got the admin site link working

--- B2BSite/B2B/urls.py
+++ B2BSite/B2B/urls.py
@@ -1,13 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
-from django.views.generic import RedirectView
-
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('results/', views.results, name='results'), #parses either the datafields or the JSON blob (throws an error if both or neither is given) and shows the results
     path('book_detail/', views.book_detail, name='book_detail'), #<slug:book_id>/ <-add this later    shows the comparison % and details on a given book???
-    #path('reporting'/, views.view_company_reports, name='view_company_reports'), #shows the reporting data for the company according to who is viewing the page
     path('company_detail/', views.CompanyDetail.as_view(), name='company_detail'), #shows the reporting data for the company according to who is viewing the page
-    path('admin/', RedirectView.as_view(url='admin/', permanent=True), name = 'admin'), #to give logged-in admins a direct link to the django-admin site
+    path('a/', include(admin.site.urls[0]), name = 'admin'), #to give logged-in admins a direct link to the django-admin site
 ]
