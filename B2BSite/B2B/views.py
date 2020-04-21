7ee1077ceee9ad803a770b7631a25fc6e6ec65a5
jme98 <jennifer.easter@eagles.oc.edu>
2020-04-20 23:48:40 -0500

    possible partial REST API???

--- B2BSite/B2B/views.py
+++ B2BSite/B2B/views.py
@@ -1,9 +1,16 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from rest_framework import generics, renderers, status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Company, Person
 
@@ -12,7 +19,6 @@ import sys
 sys.path.append('.\..\checkmate')
 import checkmate
 from checkmate import SiteBookData
-#from .checkmate import *
 # Create your views here.
 
 @login_required
@@ -24,6 +30,78 @@ def home(request):
 
     }
     return render(request, 'home.html', context = context)
+
+@login_required
+def book_detail(request):
+    book_title = "stuff"
+    book_subtitle = "lesser stuff"
+    book_series = "even lesser stuff"
+    book_volume = "a number"
+    book_authors = "a list of stuff"
+    book_format = "fancy stuff"
+    book_ISBN = "a long number"
+    book_url = "complex stuff"
+    book_match_percentage = "a fancy number"
+    book_description = "dear lord the text"
+
+    context = {
+        'book_title': book_title,
+        'book_subtitle': book_subtitle,
+        'book_series': book_series,
+        'book_volume': book_volume,
+        'book_authors': book_authors,
+        'book_format': book_format,
+        'book_ISBN': book_ISBN,
+        'book_url': book_url,
+        'book_match_percentage': book_match_percentage,
+        'book_description': book_description,
+    }
+    return render(request, 'book_detail.html', context = context)
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def book_search(request):
+    """request_body needs to be of the following form:
+
+            "queries": [ 
+                        {"author": "first author's name", "title": "first book title", "isbn": "first isbn"},
+                        {"author": "second author's name", "title": "second book title", "isbn": "second isbn"},
+                        .
+                        .
+                        .
+                        {"author": "last author's name", "title": "last book title", "isbn": "last isbn"}
+                    ]
+
+        such that the array must contain at least one element and at least one field of at least one element must not be the empty sting
+        (if the array is empty or every element contains only empty strings in its fields,
+                no meaningful results will be returned but the user (if the user is a client and not an admin) will still be billed for that search)
+    """
+    if request.user.is_staff or request.user.person is not None:        
+        if not request.user.is_staff:
+            request.user.person.search_count += 1
+            request.user.person.save()
+        
+        queries = request.body.queries
+        search_results = perform_search(queries) #get the list of SiteBookData objects that match the search queries
+        
+        if search_results is not None:
+            json_result = "results: ["
+            
+            for book in search_results:
+                json_results.append(book.to_json(), ",")
+            
+            json_result.append("]")
+
+            return response(json_result, status=200)
+
+        else:
+           res = response("\"results\":\"None\"", status=204)
+
+        return res
+
+    else:
+        return HttpResponseForbidden
 
 class Results(LoginRequiredMixin, generic.ListView):
     queryset = SiteBookData
@@ -77,33 +155,6 @@ class Results(LoginRequiredMixin, generic.ListView):
         context['TB_toggle'] = TB_toggle
         context['TB_list'] = TB_list
         return context
-
-@login_required
-def book_detail(request):
-    book_title = "stuff"
-    book_subtitle = "lesser stuff"
-    book_series = "even lesser stuff"
-    book_volume = "a number"
-    book_authors = "a list of stuff"
-    book_format = "fancy stuff"
-    book_ISBN = "a long number"
-    book_url = "complex stuff"
-    book_match_percentage = "a fancy number"
-    book_description = "dear lord the text"
-
-    context = {
-        'book_title': book_title,
-        'book_subtitle': book_subtitle,
-        'book_series': book_series,
-        'book_volume': book_volume,
-        'book_authors': book_authors,
-        'book_format': book_format,
-        'book_ISBN': book_ISBN,
-        'book_url': book_url,
-        'book_match_percentage': book_match_percentage,
-        'book_description': book_description,
-    }
-    return render(request, 'book_detail.html', context = context)
 
 class CompanyDetail(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing companies, their users, and their search counts"""
