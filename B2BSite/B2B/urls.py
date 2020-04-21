4e102e9389158e4d88d3537ece6a0126aa166848
UmbraDracona <shadowfire0117@gmail.com>
2020-04-20 20:42:29 -0500

    fixing bugs

--- B2BSite/B2B/urls.py
+++ B2BSite/B2B/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('results/', views.results, name='results'), #parses either the datafields or the JSON blob (throws an error if both or neither is given) and shows the results
+    path('results/', views.Results.as_view(), name='results'), #parses either the datafields or the JSON blob (throws an error if both or neither is given) and shows the results
     path('book_detail/', views.book_detail, name='book_detail'), #<slug:book_id>/ <-add this later    shows the comparison % and details on a given book???
     #path('reporting'/, views.view_company_reports, name='view_company_reports'), #shows the reporting data for the company according to who is viewing the page
     path('company_detail/', views.CompanyDetail.as_view(), name='company_detail'), #shows the reporting data for the company according to who is viewing the page
